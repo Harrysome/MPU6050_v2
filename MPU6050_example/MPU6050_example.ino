480d5486754423ff930f72a4fffc08dbcf234673
Harrysome Chou <65912833+Harrysome@users.noreply.github.com>
2025-03-13 04:05:44 +0800

    Update MPU6050_example.ino

--- MPU6050_example/MPU6050_example.ino
+++ MPU6050_example/MPU6050_example.ino
@@ -2,7 +2,6 @@
 
 MPU6050 mpu;
 const int addr = 0x68;   //the addr of MPU6050
-const int sen = 0;   //the sensitivity from 0 ~ 6
 
 void setup() {///
   Serial.begin(9600);
@@ -10,19 +9,29 @@ void setup() {///
   mpu.calibrate();
 }
 
+void plotData() {
+    Serial.print("RawX:"); Serial.print(mpu.Xaxis());
+    Serial.print("\tRawY:"); Serial.print(mpu.Yaxis());
+    Serial.print("\tKalX:"); Serial.print(mpu.kal_x());
+    Serial.print("\tKalY:"); Serial.println(mpu.kal_y());
+}
+
 void loop() {
   mpu.read();   //read all the num
 
-  Serial.print("mpu Xaxis = ");
-  Serial.println(mpu.Xaxis());
-  Serial.print("mpu Yaxis = ");
-  Serial.println(mpu.Yaxis());
-  Serial.print("x offset = ");
-  Serial.print(mpu.x_offset());
-  Serial.print(", y offset = ");
-  Serial.println(mpu.y_offset());
-  Serial.println("---------------------------------------------");
-  delay(250);
+  // Serial.print("mpu Xaxis = ");
+  // Serial.println(mpu.Xaxis());
+  // Serial.print("mpu Yaxis = ");
+  // Serial.println(mpu.Yaxis());
+  // Serial.println("---------------------------------------------");
+  // delay(250);
+
+  // Serial.print("x offset = ");
+  // Serial.print(mpu.x_offset());
+  // Serial.print(", y offset = ");
+  // Serial.println(mpu.y_offset());
+  // Serial.println("---------------------------------------------");
+  // delay(250);
   
   // Serial.print("mpu accX = ");
   // Serial.println(mpu.accX());
@@ -32,4 +41,13 @@ void loop() {
   // Serial.println(mpu.accZ());
   // Serial.println("---------------------------------------------");
   // delay(250);
+
+  // Serial.print("mpu kal_X = ");
+  // Serial.println(mpu.kal_x());
+  // Serial.print("mpu kal_Y = ");
+  // Serial.println(mpu.kal_y());
+  // Serial.println("---------------------------------------------");
+
+  plotData();
+  delay(100);
 }
